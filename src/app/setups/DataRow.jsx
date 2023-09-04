5cb69d6bfa197e4e4ef964ff183f7397316818c8
DiogoKahn <rm92928@fiap.com.br>
2023-09-04 10:50:21 -0300

    add fetch da api e ini do layout

---
+++ src/app/setups/DataRow.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function DataRow({ setup }) {
+    return (
+        <div id="data-row" className="group flex items-center justify-between p-2 my-2 rounded cursor-pointer hover:bg-primary">
+            <div className="flex items-center">
+                <img src={setup.file} alt={setup.name} className="w-1/2 h-12 mr-4 object-cover" />
+                <div className="flex flex-col">
+                    <span className="block font-bold">{setup.name}</span>
+                    <span className="text-sm flex-grow">{setup.description}</span>
+                    <span className="text-sm self-end">{`Preço: $${setup.price.toFixed(2)}`}</span>
+                </div>
+            </div>
+        </div>
+    );
+}
--- src/app/setups/page.js
+++ src/app/setups/page.js
@@ -1,13 +1,21 @@
 import NavBar from '../components/NavBar'
-import Image from 'next/image'
+import DataRow from "./DataRow";
 
-export default function Setups() {
+async function getSetups(){
+  const url = "http://localhost:8080/progamer/api/setups"
+  const resp = await fetch(url, { next: { revalidate: 0 } })
+  return resp.json()
+}
+
+export default async function Setups() {
+  const setups = await getSetups()
   return (
     <>
       <NavBar active={"setups"}/>
 
       <main className='bg-secondary m-20 p-12'>
         <h2>Setups</h2>
+        {setups.map(setup => <DataRow key={setup.id} setup={setup} />)}
       </main>
 
     </>
